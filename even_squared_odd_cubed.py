75a3b3b403ee9dd81f61ac5f4169cf659e50295d
diane <villalino.136534100519@depedqc.ph>
2024-04-26 06:23:54 +0800

    Define file names for output

--- even_squared_odd_cubed.py
+++ even_squared_odd_cubed.py
@@ -1,4 +1,8 @@
 # Initialize list of filename
+input_integers = "integers.txt"
+result_even_numbers = "double.txt"
+result_odd_numbers = "triple.txt"
+
 # Read integers from the file
 # Separate even and odd numbers
 # Calculate the square of even numbers and the cube of odd numbers
