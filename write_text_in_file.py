5dacdbf1b4315ca973c5805829cb61b065fd16bd
diane <villalino.136534100519@depedqc.ph>
2024-04-26 03:59:05 +0800

    User response if there are more lines

--- write_text_in_file.py
+++ write_text_in_file.py
@@ -16,5 +16,5 @@ with open(filename,"a") as file:
     # Ask the user if there are more lines
     more_lines =input("Are there more lines y/n? ").strip().lower()
 
-# User response if there are more lines
+    # User response if there are more lines
 # Print the output
