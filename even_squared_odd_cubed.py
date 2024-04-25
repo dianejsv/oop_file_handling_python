b6dc1a83496b71ae4125791102dd7e9902789bb8
diane <villalino.136534100519@depedqc.ph>
2024-04-26 06:32:15 +0800

    Read integers from the input file

--- even_squared_odd_cubed.py
+++ even_squared_odd_cubed.py
@@ -5,6 +5,8 @@ result_odd_numbers = "triple.txt"
 
 # Read integers from the file
 with open("integers.txt", "r") as file:
+    numbers = file.read().split()
+    
 # Separate even and odd numbers
 # Calculate the square of even numbers and the cube of odd numbers
 # Write the results
