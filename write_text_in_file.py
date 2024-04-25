d2c5d2faaf2d98a8ad840fbd212122c4cfb8b176
diane <villalino.136534100519@depedqc.ph>
2024-04-26 03:10:21 +0800

    Pseudocode

---
+++ write_text_in_file.py
@@ -0,0 +1,7 @@
+# Open file to append
+# Loop for multiple entries
+# Input from the user
+# Write newline
+# Ask the user if there are more lines
+# User response if there are more lines
+# Print the output
