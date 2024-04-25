7dcc24055ed326ac56d123d234a22f6128d2d8d2
diane <villalino.136534100519@depedqc.ph>
2024-04-26 02:27:01 +0800

    Added revision in line 2

--- highest_gwa_reader.py
+++ highest_gwa_reader.py
@@ -1,5 +1,5 @@
 # Open the file consisting of student names and GWAs
-with open("academic_report.txt", "r") as file:
+with open("academic_record.txt", "r") as file:
     students_record = file.readlines()
 
 # Initialize variables to store highest GWA along with student's name
