9fd4bc639519bd48d323550568e261b857191e63
diane <villalino.136534100519@depedqc.ph>
2024-04-25 18:22:16 +0800

    Newline

--- separating_odd_even_numbers.py
+++ separating_odd_even_numbers.py
@@ -4,9 +4,10 @@ print("Welcome to this Program!")
 # Read the integers from "numbers.txt"
 with open("numbers.txt", "r") as file:
     numbers = file.readlines()
+ 
 # Strip any whitespaces
 numbers = {int(num.strip()) for num in numbers}
- 
+
 # Separate odd and even integers from the list
 even_numbers = [num for num in numbers if num % 2 == 0]
 odd_numbers = [num for num in numbers if num % 2 != 0]
