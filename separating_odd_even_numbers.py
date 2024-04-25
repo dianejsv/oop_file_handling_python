5a5f6aa633a7390bb073a6371cf54affc48e20f2
diane <villalino.136534100519@depedqc.ph>
2024-04-25 18:20:25 +0800

    Create a list for odd and even numbers

--- separating_odd_even_numbers.py
+++ separating_odd_even_numbers.py
@@ -7,6 +7,9 @@ with open("numbers.txt", "r") as file:
 # Strip any whitespaces
 numbers = {int(num.strip()) for num in numbers}
 # Separate odd and even integers from the list
+even_numbers = [num for num in numbers if num % 2 == 0]
+odd_numbers = [num for num in numbers if num % 2 != 0]
+
 # Write even integers to "even.txt"
 # Write odd number integers to "odd.txt"
 # Print the output
