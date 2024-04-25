b3b19a24c323672b6a0271fddede5adc3fba69dc
diane <villalino.136534100519@depedqc.ph>
2024-04-26 00:36:36 +0800

    Pseudocode

---
+++ highest_gwa_reader.py
@@ -0,0 +1,4 @@
+# Open the file consisting of student names and GWAs
+# Initialize variables to store highest GWA along with student's name
+# Checking of the student with highest GWA
+# Display student's name with highest GWA
