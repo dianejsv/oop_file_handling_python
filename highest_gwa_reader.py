05f5ab9c134e143438892d611de5351274f98cfe
diane <villalino.136534100519@depedqc.ph>
2024-04-26 00:16:32 +0800

    Pseudocode

--- highest_gwa_reader.py
+++ highest_gwa_reader.py
@@ -1,5 +1,4 @@
 # Open the file consisting of student names and GWAs
-with open("academic_record.txt", "r") as file:
 # Initialize variables to store highest GWA along with student's name
 # Checking of the student with highest GWA
 # Display student's name with highest GWA
