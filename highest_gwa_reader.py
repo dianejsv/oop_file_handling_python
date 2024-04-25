18bd8694a9f1d67aa961076e0581c24adae46bf8
diane <villalino.136534100519@depedqc.ph>
2024-04-26 01:43:12 +0800

    Initialize with positive infinity

--- highest_gwa_reader.py
+++ highest_gwa_reader.py
@@ -3,5 +3,6 @@ with open("academic_report.txt", "r") as file:
     students_record = file.readlines()
 
 # Initialize variables to store highest GWA along with student's name
+highest_gwa = float("inf")
 # Checking of the student with highest GWA
 # Display student's name with highest GWA
