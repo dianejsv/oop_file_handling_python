5e9c82dcdca1b4f56ece7cdcf728385a1da3b6e2
diane <villalino.136534100519@depedqc.ph>
2024-04-26 04:17:55 +0800

    Pseudocode

--- write_text_in_file.py
+++ write_text_in_file.py
@@ -13,7 +13,7 @@ with open(filename,"a") as file:
         # Write newline
         file.write(line + "\n")
 
-    # Ask the user if there are more lines
+        # Ask the user if there are more lines
     more_lines =input("Are there more lines y/n? ").strip().lower()
 
     # User response if there are more lines
