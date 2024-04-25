db30efab982d684750ee37de426b9aa7a7ee32bc
diane <villalino.136534100519@depedqc.ph>
2024-04-25 17:47:26 +0800

    Pseudocode

--- separating_odd_even_numbers.py
+++ separating_odd_even_numbers.py
@@ -1,2 +1,6 @@
-
- 
+# Welcome message
+# Read the integers from "numbers.txt"
+# Separate odd and even integers from the list
+# Write even integers to "even.txt"
+# Write odd number integers to "odd.txt"
+# Print the output
